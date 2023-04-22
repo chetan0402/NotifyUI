cb8a39481af08032e2e21a7ca8be41f2bcdb3b4a
Chetan0402 <chetansharma67g@gmail.com>
2023-04-22 22:59:15 +0530

    [BUG] hotfix github actions

--- setup.py
+++ setup.py
@@ -1,12 +1,17 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
 setup(
-    name='NotifyUI',
-    version='0.0.1',
+    name="NotifyUI",
+    version="0.0.1",
     packages=find_packages(),
-    url='https://github.com/chetan0402/NotifyUI',
-    license='CC BY 4.0',
-    author='Chetan0402',
-    description='Python Notification library integrated with PyQt6',
-    requires=["PyQt6"]
+    url="https://github.com/chetan0402/NotifyUI",
+    license="CC BY 4.0",
+    author="Chetan0402",
+    description="Python Notification library integrated with PyQt6",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    requires=["PyQt6"],
 )
